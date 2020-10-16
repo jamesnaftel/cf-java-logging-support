44497418db27d1cffb819ad1434005828a9a3502
John Ewart <john@johnewart.net>
2020-10-16 14:28:09 -0700

    Update PublicKeyReader.java

--- cf-java-logging-support-servlet/src/main/java/com/sap/hcp/cf/logging/servlet/dynlog/PublicKeyReader.java
+++ cf-java-logging-support-servlet/src/main/java/com/sap/hcp/cf/logging/servlet/dynlog/PublicKeyReader.java
@@ -13,7 +13,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import com.sap.hcp.cf.logging.common.helper.Environment;
-
+// Change a Java source file.
 public class PublicKeyReader {
 
     private final static Logger LOGGER = LoggerFactory.getLogger(PublicKeyReader.class);
